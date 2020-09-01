4d3fd01fafe2b406fd334aa967e09752217d0701
Link <ylin278@wisc.edu>
2020-09-01 14:44:02 -0700

    contains duplicate hashset solution

--- Leetcode2/containDuplicate_217.java
+++ Leetcode2/containDuplicate_217.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * containDuplicate_217
@@ -8,14 +10,25 @@ public class containDuplicate_217 {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
 
-        for(int i=0; i< nums.length-1; i++){
-            if(nums[i]==nums[i+1]) return true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return true;
         }
 
         return false;
     }
 
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int x : nums) {
+            if (set.contains(x))
+                return true;
+            set.add(x);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
